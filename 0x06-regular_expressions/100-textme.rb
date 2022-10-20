e1e0f2a898c695a123f1f3645c5482889d4f30fb
kwabena sapong <kwabenasapong@gmail.com>
2022-10-20 17:02:20 +0000

    Add tasK 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?>from:)+(\+[0-9]*|[a-zA-Z]*)\X|(?>to:)+(\+[0-9]*|[a-zA-Z]*)|\X(?>flags:)+(\-[0-9]:[0-9]:)+(\-[0-9])/).join
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
