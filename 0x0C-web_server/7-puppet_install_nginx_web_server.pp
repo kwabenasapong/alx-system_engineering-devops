3e216d4d732b3aefa3818b0c0dd956ac5b66a8d8
kwabenasapong <kwabenasapong@gmail.com>
2022-12-14 22:31:43 +0000

    add task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# Script to install nginx using puppet
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/server_name _;/server_name \_\;\n\tlocation \/redirect_me \{\n\t\treturn 301 \$scheme\:\/\/\$http_host\;\n\t\}\n/" /etc/nginx/sites-enabled/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
