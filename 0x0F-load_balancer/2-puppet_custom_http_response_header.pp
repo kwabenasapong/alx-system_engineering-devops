2ac40d29816d8d1549dc15532bc6f6b9f3ecad6e
kwabenasapong <kwabenasapong@gmail.com>
2022-12-20 23:40:04 +0000

    Update task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,9 +5,18 @@ package {'nginx':
 }
 
 exec {'install':
-  command  => 'sudo apt update -y';'sudo apt -y install nginx',
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/server_name _;/server_name \_\;\n\tlocation \/redirect_me \{\n\t\treturn 301 \$scheme\:\/\/\$http_host\;\n\t\}\n/" /etc/nginx/sites-enabled/default':
+  provider => shell,
 }
 
 exec {'replace':
