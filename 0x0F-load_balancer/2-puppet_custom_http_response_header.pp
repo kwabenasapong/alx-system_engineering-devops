242546b6970bfca1b457330454e371354b6d5613
kwabenasapong <kwabenasapong@gmail.com>
2022-12-20 05:54:23 +0000

    Add files for task 1 and 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,6 +15,6 @@ exec {'sudo sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By 
 }
 
 exec {'run':
-  command  => 'sudo service nginx restart'; 'sudo update-rc.d haproxy defaults',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
