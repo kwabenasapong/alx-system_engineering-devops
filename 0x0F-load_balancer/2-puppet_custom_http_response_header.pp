7897b35239b803e31ca21949a5cd800309cddeaa
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-12-20 18:00:06 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,7 +5,7 @@ package {'nginx':
 }
 
 exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  command  => 'sudo apt update -y';'sudo apt -y install nginx',
   provider => shell,
 
 }
