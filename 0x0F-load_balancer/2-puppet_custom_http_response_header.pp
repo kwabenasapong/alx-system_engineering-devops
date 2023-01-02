c40a641ac7ad383315b86bce142376c31b2d2ba8
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-01-02 11:35:09 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,8 @@
 # Script to install nginx using puppet
-package {'nginx':
-  ensure => 'present',
-}
-
-exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  provider => shell,
-}
-
-exec {'replace':
-  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By $hostname;\n/" /etc/nginx/sites-enabled/default',
-  provider => shell,
-}
-
-exec {'run':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
 }
