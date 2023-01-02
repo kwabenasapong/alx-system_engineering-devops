cfbe380efb119babc7d22c7d82b36044c1eee550
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-01-02 11:14:00 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,15 +10,6 @@ exec {'install':
 
 }
 
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'sudo sed -i "s/server_name _;/server_name \_\;\n\tlocation \/redirect_me \{\n\t\treturn 301 \$scheme\:\/\/\$http_host\;\n\t\}\n/" /etc/nginx/sites-enabled/default':
-  provider => shell,
-}
-
 exec {'replace':
   command  => 'sudo sed -i "s/server_name _;/server_name _;\n\n\tadd_header X-Served-By \$hostname;\n/" /etc/nginx/sites-enabled/default',
   provider => shell,
