46915b353a6bdc73e5f6a1a7a5eb7e3fea45aed2
kwabenasapong <kwabenasapong@gmail.com>
2023-02-01 15:13:01 +0000

    2023-02-01 15:12:57 - README.md 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { 'replace_text_in_file':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path  => "/usr/local/bin/:/bin/",
+  command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/bin/:/bin/',
 }
