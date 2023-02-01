43f777f49c1dd8429803cf274e00a7d2ee50cd0c
kwabenasapong <kwabenasapong@gmail.com>
2023-02-01 01:16:11 +0000

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+function that queries the Reddit API and
+prints the titles of the first 10 hot posts
+listed for a given subreddit.
+'''
+import requests
+
+
+def top_ten(subreddit):
+    '''
+     Queries the Reddit API for 10 Hot posts
+     prints the list
+
+     arg: subreddit
+
+     '''
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    headers = {
+          'User-Agent': 'GetTop_10_HotPosts/v1'
+
+          }
+
+    query_response = requests.get(url, headers=headers)
+
+    if query_response.status_code != 200:
+        return 0
+    else:
+        query = query_response.json()
+        i = 0
+        while i < 10:
+            print(query.get('data').get('children')[i].
+                  get('data').get('title'))
+            i = i + 1
