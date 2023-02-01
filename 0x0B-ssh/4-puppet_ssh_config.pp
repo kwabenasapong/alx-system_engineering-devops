3419edd0f13c6ff12778014e64dab5453835e784
kwabenasapong <kwabenasapong@gmail.com>
2023-02-01 00:18:11 +0000

    Add Readme

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++
@@ -1,5 +0,0 @@
-#Configuration to add lines in ssh config
-exec { 'Append Line IdentityFile':
-  command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path    => '/etc/ssh/ssh_config',
-}
