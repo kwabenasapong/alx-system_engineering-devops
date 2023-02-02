28aaca7b28036d35ec05923559ea4877087978ad
kwabenasapong <kwabenasapong@gmail.com>
2023-02-02 07:39:23 +0000

    2023-02-02 07:39:22 - README.md 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Corrects an error in the apache2 server wp-settins.php file
+
 exec { 'replace_text_in_file':
   command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/bin/:/bin/',
