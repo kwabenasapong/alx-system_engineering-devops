e14adf96f753f7372132f0654a657fcfc0f6acc2
kwabenasapong <kwabenasapong@gmail.com>
2023-02-08 12:24:33 +0000

    2023-02-08 12:24:33 - README.md 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,6 +4,14 @@ $file_to_edit = '/etc/nginx/nginx.conf'
 
 
 exec { 'change_line_in_file':
-  command => "sed -i '2s/4/8/' ${file_to_edit}",
+  command => "sed -i '2s/4/auto/' ${file_to_edit}",
   path    => ['/bin','/usr/bin']
 }
+
+exec { 'restart_nginx':
+  command     => 'systemctl restart nginx',
+  path        => ['/bin','/usr/bin'],
+  refreshonly => true,
+}
+
+Exec['change_line_in_file'] -> Exec['restart_nginx']
