a7ad3b4b391a88ada21f53ab4e58be4b7919f133
kwabenasapong <kwabenasapong@gmail.com>
2023-02-09 07:16:30 +0000

    2023-02-09 07:16:29 - README.md x0-the_sky_is_the_limit_not.pp 1-user_limits.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Sky is the limit, let's bring that limit higher
 
 exec { 'change_line_in_file':
-  command => "sed -i 's/15/4096/' /etc/default/nginx",
+  command => "sudo sed -i 's/15/4096/' /etc/default/nginx",
   path    => ['/bin','/usr/bin']
 }
 
---
+++ 0x1B-web_stack_debugging_4/x0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,17 @@
+# Sky is the limit, let's bring that limit higher
+
+$file_to_edit = '/etc/nginx/nginx.conf'
+
+
+exec { 'change_line_in_file':
+  command => "sed -i '2s/4/auto/' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
+
+exec { 'restart_nginx':
+  command     => 'systemctl restart nginx',
+  path        => ['/bin','/usr/bin'],
+  refreshonly => true,
+}
+
+Exec['change_line_in_file'] -> Exec['restart_nginx']
