9f27496e4db7d65372c9839d6c35892d3f8d807d
kwabenasapong <kwabenasapong@gmail.com>
2023-02-09 07:18:14 +0000

    2023-02-09 07:18:14 - README.md x0-the_sky_is_the_limit_not.pp 1-user_limits.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # Sky is the limit, let's bring that limit higher
 
-exec { 'change_line_in_file':
-  command => "sudo sed -i 's/15/4096/' /etc/default/nginx",
-  path    => ['/bin','/usr/bin']
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
