9c622605adf4a99507f257cda76979a47590ff57
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2023-02-09 06:58:37 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,5 @@
 # Sky is the limit, let's bring that limit higher
 
-$file_to_edit = '/etc/nginx/nginx.conf'
-
-
-exec { 'change_line_in_file':
-  command => "sed -i '2s/4/auto/' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
-}
-
 exec { 'change_line_in_file':
   command => "sed -i 's/15/4096/' /etc/default/nginx",
   path    => ['/bin','/usr/bin']
